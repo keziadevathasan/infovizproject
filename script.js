f163f27b8e017ad2cb0b248a747389f811b24f0b
Kezia Devathasan <73145730+keziadevathasan@users.noreply.github.com>
2023-03-20 17:03:32 -0700

    script.js updated with bar charts for causes
    This version has bar charts demonstrating the causes for each emotion over the course. They are supposed to only show up when the respective emotion in the stacked chart is clicked, but I have not implemented that yet.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+//a:setup -> b:options -> c:context
+
+
+//1a:Setup for main theme river 
+
 const chartData = {
     labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'],
 		datasets: [
@@ -62,7 +68,7 @@ const chartData = {
 		]
 };
 
-// define chart options
+// 1b: main chart options
 const chartOptions = {
     onClick: function(event, elements) {
         if (elements.length > 0) {
@@ -103,7 +109,7 @@ const chartOptions = {
     }
 };
 
-
+//1c: context for main graph
 var ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
@@ -111,3 +117,363 @@ const myChart = new Chart(ctx, {
     options: chartOptions
 });
 
+/*-----------------------------------------------------------------------------*/
+//2a: setup for frustration bar
+
+const fruData = {
+    labels: ['Causes of Frustration'],
+		datasets: [
+			{
+				label: 'Perceived Deliverable Failure', //red
+				backgroundColor: 'rgba(255, 99, 132, 0.24)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1,
+				data: [10],
+				fill: 'origin'
+			},
+			{
+				label: 'Perceived Lack of Necessary Skill', //blue
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [4],
+				fill: 'origin'
+			}
+
+
+		]
+};
+
+//2b: options for frustration bar
+
+const fruOptions = { 
+	 
+		plugins: {
+			title: {
+				display: true,
+				text: 'Causes of Student Frustration'
+			}
+		
+	}
+
+   };
+
+//2c: context for frustration bar
+
+var ctx = document.getElementById('fruBar').getContext('2d');
+const fruBar = new Chart(ctx, {
+    type: 'bar',
+    data: fruData,
+    options: fruOptions
+});
+  
+/*-----------------------------------------------------------------------------*/
+
+//3a: setup for sadness bar
+
+const sadData = {
+    labels: ['Causes of Sadness'],
+		datasets: [
+			{
+				label: 'Perceived Deliverable Failure', //red
+				backgroundColor: 'rgba(255, 99, 132, 0.24)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1,
+				data: [7],
+				fill: 'origin'
+			},
+			{
+				label: 'Perceived Lack of Necessary Skill', //blue
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [14],
+				fill: 'origin'
+			}
+
+
+		]
+};
+
+//3b: options for sadness bar
+
+const sadOptions = { 
+	 
+	plugins: {
+		title: {
+			display: true,
+			text: 'Causes of Student Sadness'
+			
+		}
+	
+}
+
+};
+
+//3c: context for sadness bar
+
+var ctx = document.getElementById('sadBar').getContext('2d');
+const sadBar = new Chart(ctx, {
+    type: 'bar',
+    data: sadData,
+    options: sadOptions
+});
+  
+
+/*-----------------------------------------------------------------------------*/
+
+//4a: setup for happiness bar
+
+const hapData = {
+    labels: ['Causes of Happiness'],
+		datasets: [
+			{
+				label: 'Perceived Deliverable Failure', //red
+				backgroundColor: 'rgba(255, 99, 132, 0.24)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1,
+				data: [7],
+				fill: 'origin'
+			},
+			{
+				label: 'Perceived Lack of Necessary Skill', //blue
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [14],
+				fill: 'origin'
+			}
+
+
+		]
+};
+
+//4b: options for sadness bar
+
+const hapOptions = { 
+	 
+	plugins: {
+		title: {
+			display: true,
+			text: 'Causes of Student Happiness'
+		}
+	
+}
+
+};
+
+//4c: context for sadness bar
+
+var ctx = document.getElementById('hapBar').getContext('2d');
+const hapBar = new Chart(ctx, {
+    type: 'bar',
+    data: hapData,
+    options: hapOptions
+});
+  
+
+/*-----------------------------------------------------------------------------*/
+
+//5a: setup for excitement bar
+
+const excData = {
+    labels: ['Causes of Excitement'],
+		datasets: [
+			{
+				label: 'Perceived Deliverable Failure', //red
+				backgroundColor: 'rgba(255, 99, 132, 0.24)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1,
+				data: [17],
+				fill: 'origin'
+			},
+			{
+				label: 'Perceived Lack of Necessary Skill', //blue
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [4],
+				fill: 'origin'
+			}
+
+
+		]
+};
+
+//5b: options for excitement bar
+
+const excOptions = { 
+	 
+	plugins: {
+		title: {
+			display: true,
+			text: 'Causes of Student Excitement'
+		}
+	
+}
+
+};
+
+//5c: context for excitement bar
+
+var ctx = document.getElementById('excBar').getContext('2d');
+const excBar = new Chart(ctx, {
+    type: 'bar',
+    data: excData,
+    options: excOptions
+});
+
+
+/*-----------------------------------------------------------------------------*/
+
+//6a: setup for fear bar
+
+const feaData = {
+    labels: ['Causes of Fear'],
+		datasets: [
+			{
+				label: 'Perceived Deliverable Failure', //red
+				backgroundColor: 'rgba(255, 99, 132, 0.24)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1,
+				data: [17],
+				fill: 'origin'
+			},
+			{
+				label: 'Perceived Lack of Necessary Skill', //blue
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [4],
+				fill: 'origin'
+			}
+
+
+		]
+};
+
+//6b: options for fear bar
+
+const feaOptions = { 
+	 
+	plugins: {
+		title: {
+			display: true,
+			text: 'Excitement'
+		}
+	
+}
+
+};
+
+//6c: context for fear bar
+
+var ctx = document.getElementById('feaBar').getContext('2d');
+const feaBar = new Chart(ctx, {
+    type: 'bar',
+    data: feaData,
+    options: feaOptions
+});
+
+
+/*-----------------------------------------------------------------------------*/
+
+//7a: setup for dissapointment bar
+
+const disData = {
+    labels: ['Causes of Dissapointment'],
+		datasets: [
+			{
+				label: 'Perceived Deliverable Failure', //red
+				backgroundColor: 'rgba(255, 99, 132, 0.24)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1,
+				data: [17],
+				fill: 'origin'
+			},
+			{
+				label: 'Perceived Lack of Necessary Skill', //blue
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [4],
+				fill: 'origin'
+			}
+
+
+		]
+};
+
+//7b: options for dissapointment bar
+
+const disOptions = { 
+	 
+	plugins: {
+		title: {
+			display: true,
+			text: 'Causes of Student Dissapointment'
+		}
+	
+}
+
+};
+
+//7c: context for dissapointment bar
+
+var ctx = document.getElementById('disBar').getContext('2d');
+const disBar = new Chart(ctx, {
+    type: 'bar',
+    data: disData,
+    options: disOptions
+});
+
+/*-----------------------------------------------------------------------------*/
+
+//8a: setup for surprise bar
+
+const surData = {
+    labels: ['Causes of Surprise'],
+		datasets: [
+			{
+				label: 'Perceived Deliverable Failure', //red
+				backgroundColor: 'rgba(255, 99, 132, 0.24)',
+				borderColor: 'rgba(255, 99, 132, 1)',
+				borderWidth: 1,
+				data: [17],
+				fill: 'origin'
+			},
+			{
+				label: 'Perceived Lack of Necessary Skill', //blue
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor: 'rgba(54, 162, 235, 1)',
+				borderWidth: 1,
+				data: [4],
+				fill: 'origin'
+			}
+
+
+		]
+};
+
+//8b: options for surprise bar
+
+const surOptions = { 
+	 
+	plugins: {
+		title: {
+			display: true,
+			text: 'Causes of Student Surprise'
+		}
+	
+}
+
+};
+
+//8c: context for surprise bar
+
+var ctx = document.getElementById('surBar').getContext('2d');
+const surBar = new Chart(ctx, {
+    type: 'bar',
+    data: surData,
+    options: surOptions
+});
